8a07279f9d697bf29ff81c5dc7fb08096e6c0eb2
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2023-12-30 10:51:02 +0530

    Setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    """Get requirements from a file."""
+
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+
+setup(
+    name='Second Hand Car Price Prediction',
+    version='0.0.1',
+    description="Here in this we predict the price of a second hand car price",
+    author="Dipayan Majumder",
+    author_email="majumderd007@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
