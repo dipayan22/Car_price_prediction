0e3c256af7674c075a5903f2c703ae610a89b40e
Dipayan Majumder <110282003+dipayan22@users.noreply.github.com>
2024-01-12 20:16:45 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion,DataIngestionConfig
 import sys
 
 if __name__=='__main__':
     logging.info("The Execution has Started")
 
     try:
-        a=1/0
+       
+       data_ingestion=DataIngestion()
+       data_ingestion.initiate_data_ingestion()
+
+       
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
